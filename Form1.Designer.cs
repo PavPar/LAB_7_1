86b24e31a7eb3570b3fb1bdbbc4edd06feab90fe
PavPar <ParamonPav@yandex.ru>
2020-05-14 21:39:53 +0300

    feat: Project is done by magic

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace LAB_7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(363, 262);
+            this.button1.MaximumSize = new System.Drawing.Size(272, 90);
+            this.button1.MinimumSize = new System.Drawing.Size(50, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(674, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(674, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(674, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 670);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
+            this.MinimumSize = new System.Drawing.Size(840, 640);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_7_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы нажали на кнопку, вау", "Ты победил, но какой ценой?");
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //label3.Text = "W: " + Form1.ActiveForm.Size;
+            Button btn = button1;
+            Point pos = btn.PointToClient(Cursor.Position);
+            label1.Text = "btn_loc: " + PointToClient(button1.Location);
+            label2.Text = "C_X_Test: " + this.PointToClient(Cursor.Position);
+            Point center = new Point(((pos.X - (btn.Size.Width / 2)) + Math.Sign(pos.X - (btn.Size.Width / 2))), ((pos.Y - (btn.Size.Height / 2)) + Math.Sign(pos.Y - (btn.Size.Height / 2))));
+            label3.Text = "Btn_Curs: " + Math.Abs(center.X / (btn.Size.Width / 2)) + " : " + Math.Abs(center.Y / (btn.Size.Height / 2));
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            Point pos = btn.PointToClient(Cursor.Position);
+            Point center = new Point(((pos.X - (btn.Size.Width / 2)) + Math.Sign(pos.X - (btn.Size.Width / 2))), ((pos.Y - (btn.Size.Height / 2)) + Math.Sign(pos.Y - (btn.Size.Height / 2))));
+            
+           
+            if ((float)Math.Abs(center.X)/(btn.Size.Width / 2) > (float)Math.Abs(center.Y) / (btn.Size.Height / 2))
+            {
+                btn.Location = new Point(btn.Location.X + center.X + Math.Sign(-center.X) * (btn.Size.Width / 2), btn.Location.Y);
+            }
+            else
+            {
+                if ((float)Math.Abs(center.X) / (btn.Size.Width / 2) == (float)Math.Abs(center.Y) / (btn.Size.Height / 2))
+                {
+                    btn.Location = new Point(btn.Location.X + center.X + Math.Sign(-center.X) * (btn.Size.Width / 2), btn.Location.Y + center.Y + Math.Sign(-center.Y) * (btn.Size.Height / 2));
+                }
+                else
+                {
+                    btn.Location = new Point(btn.Location.X, btn.Location.Y + center.Y + Math.Sign(-center.Y) * (btn.Size.Height / 2));
+                }
+            }
+
+            try
+            {
+                if (btn.Location.X > ActiveForm.Size.Width - 2*btn.Size.Width)
+                {
+                    btn.Location = new Point(Size.Width - 2 * btn.Size.Width, btn.Location.Y);
+                }
+                if (btn.Location.X < 0 + btn.Size.Width)
+                {
+                    btn.Location = new Point(0 + btn.Size.Width, btn.Location.Y);
+                }
+                if (btn.Location.Y > Size.Height - 2 * btn.Size.Height)
+                {
+                    btn.Location = new Point(btn.Location.X, Size.Height - 2 * btn.Size.Height);
+                }
+                if (btn.Location.Y < 0 + btn.Size.Height)
+                {
+                    btn.Location = new Point(btn.Location.X, 0 + btn.Size.Height);
+                }
+            }
+            catch
+            {
+                //idk
+            }
+         
+        }
+    }
+
+}
